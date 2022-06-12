fcf8b32bc6e3ba765988090fe3f30d27ef8e13d0
Antonio Barrera <jeantonio.barrera@gmail.com>
2022-06-12 12:02:06 -0600

    configuration of project resources handler and added home products listing feature

--- src/main/java/com/ecommerce/controller/AdminController.java
+++ src/main/java/com/ecommerce/controller/AdminController.java
@@ -4,7 +4,10 @@
  */
 package com.ecommerce.controller;
 
+import com.ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,9 +18,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+    
+    @Autowired
+    private ProductService productService;
 
     @GetMapping("")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("products", productService.getAll());
         return "admin/home";
     }
 }
