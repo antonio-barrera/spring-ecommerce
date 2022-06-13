3ee5aff00c02a3a22e599985a99b67bb7e08a232
Antonio Barrera <jeantonio.barrera@gmail.com>
2022-06-13 00:23:48 -0600

    creation of shopping cart view

--- src/main/java/com/ecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/controller/HomeController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -38,5 +39,10 @@ public class HomeController {
         model.addAttribute("product", productService.get(id));
         LOGGER.info("Product: {}", id);
         return "user/product";
+    }
+    
+    @PostMapping("cart")
+    public String addCart() {
+        return "user/cart";
     }
 }
