7d8ad773092cc16671352eea11e2b87bc0fa488f
Antonio Barrera <jeantonio.barrera@gmail.com>
2022-06-12 23:28:21 -0600

    creation of see product details feature

--- src/main/java/com/ecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/controller/HomeController.java
@@ -5,10 +5,13 @@
 package com.ecommerce.controller;
 
 import com.ecommerce.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -19,6 +22,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HomeController {
     
+    private final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+    
     @Autowired
     private ProductService productService;
     
@@ -26,5 +31,11 @@ public class HomeController {
     public String home(Model model) {
         model.addAttribute("products", productService.getAll());
         return "user/home";
+    }
+    
+    @GetMapping("product/{id}")
+    public String product(@PathVariable Integer id) {
+        LOGGER.info("Product: {}", id);
+        return "user/product";
     }
 }
